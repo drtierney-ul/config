64ad3f0d2c04c2754c77938c1b094b593e3ff9b3
drtierney-ul <119501216+drtierney-ul@users.noreply.github.com>
2024-09-19 14:20:18 +0100

    Update nginx.sh
    Setting frontend to NonInteractive

--- nginx.sh
+++ nginx.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Set the DEBIAN_FRONTEND to noninteractive to avoid prompts
+#export DEBIAN_FRONTEND=noninteractive
+echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+
 # Update and upgrade package lists
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get upgrade -y
 
 # Install Nginx
