546a4e53671aa97e291ece93bb8e646986543a17
drtierney-ul <119501216+drtierney-ul@users.noreply.github.com>
2024-09-19 14:14:40 +0100

    Update nginx.sh
    Added package update/upgrade steps

--- nginx.sh
+++ nginx.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+# Update and upgrade package lists
+sudo apt-get update
+sudo apt-get upgrade -y
+
 # Install Nginx
 sudo apt-get install -y nginx
-systemctl enable nginx
-systemctl start nginx
+
+# Enable and start Nginx
+sudo systemctl enable nginx
+sudo systemctl start nginx
 
 # Create a simple HTML file with "Hello from {hostname}"
 echo "Hello from ${HOSTNAME}!" | sudo tee /var/www/html/index.html
